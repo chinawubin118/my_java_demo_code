43673c9ff3d644cd4b8b50b00bee52ca5fa0d0c1
chinawubin118 <565696580@qq.com>
2017-04-07 23:50:01 +0800

    java_jdbc_1

--- WebRoot/JdbcJsp.jsp
+++ WebRoot/JdbcJsp.jsp
@@ -21,5 +21,77 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
   
   <body>
   
+  <p><b>------jdbc-api详解------</b></p>
+  	所有的包 都是 java.sql 或者 javax.sql<br><br>
+	
+	DriverManager(类):管理了一组jdbc的操作<br><br>
+	常用方法:<br>
+	了解:注册驱动.<br>
+	static void registerDriver(Driver driver):<br>
+	驱动注册了两次.我们只需要将静态代码块执行一次,类被加载到内存中会执行静态代码块,并且只执行一次.<br>
+	现在只需要将类加载到内存中即可:<br>
+	方式1:★Class.forName("全限定名");//包名+类名   com.mysql.jdbc.Driver<br>
+	方式2:类名.class;<br>
+	方式3:对象.getClass();<br><br>
+ 
+	掌握:获取连接.<br>
+	static Connection getConnection(String url, String user, String password);<br>
+	参数1:告诉我们连接什么类型的数据库及连接那个数据库<br>
+	协议:数据库类型:子协议 参数<br>
+	mysql:	jdbc:mysql://localhost:3306/数据库名称<br>
+	oracle:	jdbc:oracle:thin@localhost:1521@实例<br>
+	参数2:账户名 root.....参数3:密码<br><br>
+	
+	(了解)java.sql.Driver;...是一个接口...驱动<br><br>
+	
+	Connection(接口):连接 <br><br>
+	
+	常用方法:<br>
+	获取语句执行者:<br>
+	(了解)Statement createStatement() :获取普通的语句执行者 ,会出现sql注入问题<br>
+	★PreparedStatement prepareStatement(String sql) :获取预编译语句执行者.<br>
+	(了解)CallableStatement prepareCall(String sql):获取调用存储过程的语句执行者.<br><br>
+
+	了解:<br>
+	setAutoCommit(false) :手动开启事务.<br>
+	commit():提交事务.<br>
+	rollback():事务回滚.<br><br>
+	
+	Statement(接口,基本不用):语句执行者.<br>
+	PreparedStatement(接口):预编译语句执行者.<br>
+	常用方法:
+	设置参数:setXxx(int 第几个问号,Object 实际参数);<br>
+						 setInt();<br>
+						 setString();<br>
+						 setObject();<br><br>
+			
+	执行sql:
+	ResultSet executeQuery():执行 r语句(查询),返回值:结果集;<br>
+	int executeUpdate():执行cud语句,返回值:影响的行数;<br><br>
+ 
+	ResultSet(结果):查询出来的结果集(执行查询语句之后返回的结果);<br>
+	常用方法:
+	boolean next():判断是否有下一条记录,若有返回true且将光标移到下一行,若没有,则返回false,光标一开始处于第一条记录的上面;<br><br>
+				
+	获取具体内容:<br>
+	getXxx(int|string):若参数为int:是指第几列;若参数为string:是指列名(字段名);<br>
+
+	常用方法:<br>
+						getInt();<br>
+						getString();也可以获取int值;<br>
+						getObject();可以获取任意;<br>
+						
+	<p><b>------常见的配置文件格式------</b></p>
+	
+	1.Properties:里面内容的格式 key=value;<br>
+	2.xml;<br><br>
+	
+	在src目录下创建properties文件,可以通过 ResourceBundle工具快速获取里面的配置信息;<br>
+	使用步骤:<br>
+	1.获取ResourceBundle对象:<br>
+	static ResourceBundle getBundle("文件名称不带后缀名");<br>
+	2.通过ResourceBundle 对象获取配置信息;<br>
+	String getString(String key):通过执行key获取指定的value;<br>
+	
   </body>
 </html>
---
+++ src/jdbc/Jdbc1Demo.java
@@ -0,0 +1,147 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.mysql.jdbc.Driver;
+
+public class Jdbc1Demo {
+	@Test
+	public void f2() throws Exception {
+
+		// 注册驱动
+		 Class.forName("com.mysql.jdbc.Driver");
+		
+		// Connection conn = DriverManager
+		// .getConnection("jdbc:mysql://168t2130v4.51mypc.cn:43974/lotte",
+		// "root", "develop123");
+		Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/lotte", "root", "develop123");
+
+		// 编写sql
+		String sql = "select * from products";
+
+		// 创建语句执行者
+		PreparedStatement st = conn.prepareStatement(sql);
+
+		// 设置参数
+
+		// 执行sql
+		ResultSet rs = st.executeQuery();
+
+		// 处理结果
+		while (rs.next()) {
+			System.out.println(rs.getString("pname") + "::"
+					+ rs.getString("price"));
+		}
+
+		// 释放资源.
+		rs.close();
+		st.close();
+		conn.close();
+	}
+
+	// 插入一条数据
+	@Test
+	public void f3() {
+		Connection conn = null;
+		ResultSet rs = null;
+		PreparedStatement st = null;
+
+		try {
+			// 获取连接
+			conn = JdbcUtils.getConnection();
+
+			// 编写sql
+			String sql = "insert into  category values(?,?)";
+
+			// 获取语句执行者
+			st = conn.prepareStatement(sql);
+
+			// 设置参数
+			st.setString(1, "c006");
+			st.setString(2, "户外");
+
+			// 执行sql
+			int i = st.executeUpdate();
+
+			// 处理结果
+			if (i == 1) {
+				System.out.println("success");
+			} else {
+				System.out.println("fail");
+			}
+
+		} catch (Exception e) {
+			System.out.println("插入数据发生异常");
+		} finally {
+			// 释放资源
+			JdbcUtils.closeResource(conn, st, rs);
+		}
+
+	}
+
+	// 更新
+	@Test
+	public void f11() {
+		Connection conn = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = JdbcUtils.getConnection();
+			String sql = "update category set cname = ? where cid = ?";
+			st = conn.prepareStatement(sql);
+			st.setString(1, "手机");
+			st.setString(2, "c006");
+
+			int i = st.executeUpdate();
+
+			if (i == 1) {
+				System.out.println("success");
+			} else {
+				System.out.println("fail");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 释放资源
+			JdbcUtils.closeResource(conn, st, rs);
+		}
+	}
+
+	// 删除
+	@Test
+	public void f12() {
+		Connection conn = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = JdbcUtils.getConnection();
+			String sql = "delete from category where cid = ?";
+			st = conn.prepareStatement(sql);
+
+			st.setString(1, "c006");
+
+			int i = st.executeUpdate();
+			if (i == 1) {
+				System.out.println("success");
+			} else {
+				System.out.println("fail");
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResource(conn, st, rs);
+		}
+	}
+}
